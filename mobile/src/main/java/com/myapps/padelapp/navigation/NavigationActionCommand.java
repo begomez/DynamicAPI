05ba83acd0c63725de8803844b004e34ccff2199
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-07 07:13:51 +0100

    Add primary navigation support

---
+++ mobile/src/main/java/com/myapps/padelapp/navigation/NavigationActionCommand.java
@@ -0,0 +1,13 @@
+package com.myapps.padelapp.navigation;
+
+
+/**
+ * Created by bernatgomez on 7/3/17.
+ */
+public class NavigationActionCommand implements INavigation {
+
+    @Override
+    public void navigate() {
+
+    }
+}
