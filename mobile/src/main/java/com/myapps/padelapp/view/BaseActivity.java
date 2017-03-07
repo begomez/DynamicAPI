cf2c50f3b36010b8dad268da2a87ea463e96090f
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-07 07:07:24 +0100

    Implement methods on BaseActiv

--- mobile/src/main/java/com/myapps/padelapp/view/BaseActivity.java
+++ mobile/src/main/java/com/myapps/padelapp/view/BaseActivity.java
@@ -4,14 +4,83 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.myapps.padelapp.navigation.INavigation;
+import com.myapps.padelapp.utils.AndroidLoggerUtils;
+
 /**
  * Created by bernatgomez on 6/3/17.
  */
 
 public class BaseActivity extends AppCompatActivity {
 
+    private static final String TAG = BaseActivity.class.getSimpleName();
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// LIFE CYCLE
+/////////////////////////////////////////////////////////////////////////////////////////
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AndroidLoggerUtils.logMsg(TAG, "onCreate()");
+
+        this.configWindow();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        AndroidLoggerUtils.logMsg(TAG, "onDestroy()");
+    }
+
+    /**
+     * Request window features inside this method
+     */
+    protected void configWindow() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AndroidLoggerUtils.logMsg(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        AndroidLoggerUtils.logMsg(TAG, "onPause()");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        AndroidLoggerUtils.logMsg(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        AndroidLoggerUtils.logMsg(TAG, "onStop()");
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// NAVIGATION
+/////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     *
+     * @param cmd
+     */
+    protected void navigateToNext(INavigation cmd) {
+        cmd.navigate();
+    }
+
 }
