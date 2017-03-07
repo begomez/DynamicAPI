a227a41bf5dd8f1617dc3c51a4c4ec088afc234a
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-07 07:34:55 +0100

    Modify Navigation hierarchy

--- mobile/src/main/java/com/myapps/padelapp/navigation/BaseNavigation.java
+++ mobile/src/main/java/com/myapps/padelapp/navigation/BaseNavigation.java
@@ -1,13 +1,10 @@
 package com.myapps.padelapp.navigation;
 
-
 /**
  * Created by bernatgomez on 7/3/17.
  */
-public class NavigationActionCommand implements INavigation {
 
+public abstract class BaseNavigation implements INavigation {
     @Override
-    public void navigate() {
-
-    }
+    public abstract void navigate();
 }
