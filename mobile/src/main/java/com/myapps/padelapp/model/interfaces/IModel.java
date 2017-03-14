4897263c96b6e23cbaad495fcdfc0163bcd3547f
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-14 07:32:16 +0100

    Change parent on base model class

--- mobile/src/main/java/com/myapps/padelapp/model/interfaces/IModel.java
+++ mobile/src/main/java/com/myapps/padelapp/model/interfaces/IModel.java
@@ -2,9 +2,11 @@ package com.myapps.padelapp.model.interfaces;
 
 import android.os.Parcelable;
 
+import java.io.Serializable;
+
 /**
  * Created by bernatgomez on 2/3/17.
  */
 
-public interface IModel extends Parcelable {
+public interface IModel extends Serializable {
 }
