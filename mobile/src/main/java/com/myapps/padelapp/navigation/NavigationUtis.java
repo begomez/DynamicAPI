501e7fb01f4f0bb164208e5ebec6c3de313351ca
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-19 20:40:56 +0100

    Add launch fragment in navigation utils

--- mobile/src/main/java/com/myapps/padelapp/navigation/NavigationUtis.java
+++ mobile/src/main/java/com/myapps/padelapp/navigation/NavigationUtis.java
@@ -3,6 +3,10 @@ package com.myapps.padelapp.navigation;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.myapps.padelapp.view.BaseFragment;
 
 /**
  * Created by bernatgomez on 7/3/17.
@@ -10,6 +14,29 @@ import android.os.Bundle;
 
 public class NavigationUtis {
 
+    /**
+     *
+     * @param mgr
+     * @param frag
+     * @param contentFrame
+     */
+    public static void launchFragment(FragmentManager mgr, BaseFragment frag, int contentFrame) {
+        FragmentTransaction trans = mgr.beginTransaction();
+
+        trans.replace(contentFrame, frag);
+
+        trans.addToBackStack(frag.getComponentIdentifier());
+
+        trans.commit();
+
+        mgr.executePendingTransactions();
+    }
+
+    /**
+     *
+     * @param cntxt
+     * @param c
+     */
     public static void navigateToActivity(Context cntxt, Class c) {
         Intent i = new Intent(cntxt, c);
 
