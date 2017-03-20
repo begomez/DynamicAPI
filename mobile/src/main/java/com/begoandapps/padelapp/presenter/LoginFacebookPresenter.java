a53f1d1fbd6d7fc46919b01b26c6b4d16452f0e7
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-20 21:54:41 +0100

    Create empty presenter

---
+++ mobile/src/main/java/com/begoandapps/padelapp/presenter/LoginFacebookPresenter.java
@@ -0,0 +1,20 @@
+package com.begoandapps.padelapp.presenter;
+
+import com.begoandapps.padelapp.view.interfaces.ILoginFacebookView;
+
+/**
+ * Created by bernatgomez on 20/3/17.
+ */
+
+public class LoginFacebookPresenter extends BasePresenter {
+
+    ILoginFacebookView view;
+
+    public LoginFacebookPresenter() {
+
+    }
+
+    public void doLogin() {
+
+    }
+}
