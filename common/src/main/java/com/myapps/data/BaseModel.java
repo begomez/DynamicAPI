432d8bcccb2c0b62b2beb1eea201fead9055781d
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-20 19:37:11 +0100

    Modify model in common module

--- common/src/main/java/com/myapps/data/BaseModel.java
+++ common/src/main/java/com/myapps/data/BaseModel.java
@@ -1,8 +1,11 @@
 package com.myapps.data;
 
+import java.io.Serializable;
+
 /**
  * Created by bernatgomez on 20/3/17.
  */
 
-public class BaseModel {
+public class BaseModel implements Serializable {
+
 }
