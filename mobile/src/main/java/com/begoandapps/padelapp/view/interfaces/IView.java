da568b6456168db7e9227a690f15d8d1b2cceb56
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-21 19:55:00 +0100

    Modify view hierarchy

--- mobile/src/main/java/com/begoandapps/padelapp/view/interfaces/IView.java
+++ mobile/src/main/java/com/begoandapps/padelapp/view/interfaces/IView.java
@@ -1,10 +1,14 @@
 package com.begoandapps.padelapp.view.interfaces;
 
+import android.content.Context;
+
 /**
  * Created by bernatgomez on 2/3/17.
  */
 
 public interface IView {
+
+    public Context getContext();
 
     public void showLoading();
 
