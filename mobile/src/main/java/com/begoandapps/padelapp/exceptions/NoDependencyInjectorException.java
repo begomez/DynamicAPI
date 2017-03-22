4b7230ff7978e20dd37459fd4b835ea9501f8c80
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-22 20:16:27 +0100

    Create custom exception

---
+++ mobile/src/main/java/com/begoandapps/padelapp/exceptions/NoDependencyInjectorException.java
@@ -0,0 +1,16 @@
+package com.begoandapps.padelapp.view;
+
+/**
+ * Created by bernatgomez on 22/3/17.
+ */
+
+public class NoDependencyInjectorException extends Exception {
+
+    public NoDependencyInjectorException() {
+
+    }
+
+    public NoDependencyInjectorException(String msg) {
+        super(msg);
+    }
+}
