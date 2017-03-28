e36d97f02deb7e6a920885c50cffb163cb0a2cd3
Bernat Gomez <bernat.gomez@mobetia.com>
2017-03-28 23:29:49 +0200

    Create use case sample in domain module

--- domain/src/main/java/com/begoandapps/padel/usecases/login/RegisterUseCaseImpl.java
+++ domain/src/main/java/com/begoandapps/padel/usecases/login/RegisterUseCaseImpl.java
@@ -1,6 +1,11 @@
 package com.begoandapps.padel.usecases.login;
 
 import com.begoandapps.padel.usecases.BaseUseCase;
+import com.squareup.otto.Bus;
+
+import javax.inject.Inject;
+
+import rest.RestDataSource;
 
 /**
  * Created by bernatgomez on 20/3/17.
@@ -11,6 +16,12 @@ public class RegisterUseCaseImpl extends BaseUseCase implements RegisterUseCase 
 
     public RegisterUseCaseImpl() {
         super();
+    }
+
+
+    //@Inject
+    public RegisterUseCaseImpl(Bus bus, RestDataSource rest) {
+        super(bus, rest);
     }
 
     @Override
