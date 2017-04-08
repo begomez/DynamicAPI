40300092145a398224f12790bc012fd1d6be6aa3
Bernat Gomez <bernat.gomez@mobetia.com>
2017-04-08 08:28:55 +0200

    Add module container in rest data source class

--- data/src/main/java/rest/RestModuleDataSource.java
+++ data/src/main/java/rest/RestModuleDataSource.java
@@ -4,7 +4,10 @@ import com.squareup.otto.Bus;
 
 import javax.inject.Inject;
 
+import rest.exceptions.ModuleNotFoundException;
 import rest.interfaces.IDataSource;
+import rest.interfaces.IModule;
+import rest.interfaces.IModuleContainer;
 import retrofit2.Retrofit;
 
 /**
@@ -19,6 +22,13 @@ public class RestModuleDataSource implements IDataSource {
 
     private GithubModuleImpl github;
 
+    private ModuleContainer moduleContainer;
+
+
+////////////////////////////////////////////////////////////////////////////////////
+//  CONSTRUCTOR
+////////////////////////////////////////////////////////////////////////////////////
+
     @Inject
     public RestModuleDataSource(Bus bus, Retrofit retrofit) {
         this.bus = bus;
@@ -26,10 +36,36 @@ public class RestModuleDataSource implements IDataSource {
         this.retrofit = retrofit;
 
         this.github = new GithubModuleImpl(bus, retrofit);
+
+        this.moduleContainer = new ModuleContainer();
+
+        this.fakeRegistration();
+    }
+
+    public void fakeRegistration() {
+        this.moduleContainer.registerModule(IModuleContainer.MODULE_GITHUB, new ModuleContainerEntry(new GithubModuleImpl(bus, retrofit)));
+        this.moduleContainer.registerModule(IModuleContainer.MODULE_ANOTHER, new ModuleContainerEntry(new AnotherModuleImpl(bus, retrofit)));
     }
 
     public void sampleCall() {
+        //this.getModuleContainer().getModuleByName(IModuleContainer.MODULE_GITHUB).getEntry().getEntry()
         this.github.sampleCall("status:open");
     }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+//  ACCESSORS
+////////////////////////////////////////////////////////////////////////////////////
+
+    public ModuleContainer getModuleContainer() {
+        return moduleContainer;
+    }
+
+    public void setModuleContainer(ModuleContainer moduleContainer) {
+        this.moduleContainer = moduleContainer;
+    }
+
+
 
 }
