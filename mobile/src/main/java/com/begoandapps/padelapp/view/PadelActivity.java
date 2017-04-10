297eec2d23fed5215485960bdf2bfbb1dea03d80
Bernat Gomez <bernat.gomez@mobetia.com>
2017-04-10 19:36:24 +0200

    Link search from dashboard through test button

--- mobile/src/main/java/com/begoandapps/padelapp/view/PadelActivity.java
+++ mobile/src/main/java/com/begoandapps/padelapp/view/PadelActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.begoandapps.padelapp.R;
+import com.begoandapps.padelapp.navigation.NavigationUtils;
 import com.begoandapps.padelapp.view.BaseActivity;
+
+import butterknife.OnClick;
 
 public class PadelActivity extends BaseActivity {
 
@@ -28,5 +31,15 @@ public class PadelActivity extends BaseActivity {
     @Override
     public void onMainBtnClick(View v) {
         super.onMainBtnClick(v);
+
+    }
+
+    @OnClick(R.id.testButton)
+    public void onTestBtnClick(View v){
+        this.navigateToSearch();
+    }
+
+    private void navigateToSearch() {
+        NavigationUtils.navigateToActivity(this.getApplicationContext(), SearchActivity.class);
     }
 }
