c93f614658f2f0834601541e3bb4ba4771fdcf66
Bernat Gomez <bernat.gomez@mobetia.com>
2017-04-11 19:56:52 +0200

    Modify padel activity

--- mobile/src/main/java/com/begoandapps/padelapp/view/PadelActivity.java
+++ mobile/src/main/java/com/begoandapps/padelapp/view/PadelActivity.java
@@ -1,17 +1,35 @@
 package com.begoandapps.padelapp.view;
 
+
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.begoandapps.padelapp.R;
+import com.begoandapps.padelapp.components.AppTabBar;
 import com.begoandapps.padelapp.navigation.NavigationUtils;
 import com.begoandapps.padelapp.navigation.interfaces.INavigation;
+import com.begoandapps.padelapp.utils.MessageUtils;
 import com.begoandapps.padelapp.view.BaseActivity;
+import com.begoandapps.padelapp.view.interfaces.ITabBar;
 
+import butterknife.BindView;
 import butterknife.OnClick;
+import butterknife.Optional;
 
-public class PadelActivity extends BaseActivity {
+
+/**
+ *
+ */
+public class PadelActivity
+    extends BaseActivity
+    implements ITabBar {
+
+    @Nullable
+    @BindView(R.id.tab_bar)
+    protected AppTabBar tabBar;
+
 
 //////////////////////////////////////////////////////////////////////////////////
 // LIFE CYCLE
@@ -26,25 +44,40 @@ public class PadelActivity extends BaseActivity {
     }
 
 //////////////////////////////////////////////////////////////////////////////////
-// INTERACTION
+// ARCHITECTURE
 //////////////////////////////////////////////////////////////////////////////////
 
     @Override
-    public void onMainBtnClick(View v) {
-        super.onMainBtnClick(v);
+    public void launchContentFragment() {
+        super.launchContentFragment();
 
+        NavigationUtils.launchFragment(this.getSupportFragmentManager(), NoReservationFragment.newInstance(), BaseActivity.CONTENT_FRAME,  false);
     }
 
-    @OnClick(R.id.testButton)
-    public void onTestBtnClick(View v){
-        this.navigateToSearch();
+    @Override
+    public void configViews() {
+        super.configViews();
+
+        this.configTabBar();
+    }
+
+    private void configTabBar() {
+        if (this.tabBar != null) {
+            this.tabBar.setCallback(this);
+        }
     }
 
 //////////////////////////////////////////////////////////////////////////////////
-// NAVIGATION
+// ITABBAR IMPL
 //////////////////////////////////////////////////////////////////////////////////
 
-    private void navigateToSearch() {
+    @Override
+    public void onGeoClick() {
+        MessageUtils.showToast(this, "Geo");
+    }
+
+    @Override
+    public void onSearchClick() {
         new INavigation() {
             @Override
             public void navigate() {
@@ -52,5 +85,10 @@ public class PadelActivity extends BaseActivity {
             }
         }.navigate();
 
+    }
+
+    @Override
+    public void onProfileClick() {
+        MessageUtils.showToast(this, "Profile");
     }
 }
