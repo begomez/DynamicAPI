8fd3f6b4181451e3ae96369c2957b801bb8d6cef
Bernat Gomez <bernat.gomez@mobetia.com>
2017-04-11 19:56:18 +0200

    Modify tab bar

---
+++ mobile/src/main/java/com/begoandapps/padelapp/view/interfaces/ITabBar.java
@@ -0,0 +1,11 @@
+package com.begoandapps.padelapp.view.interfaces;
+
+/**
+ * Created by bernatgomez on 11/4/17.
+ */
+
+public interface ITabBar {
+    public void onGeoClick();
+    public void onSearchClick();
+    public void onProfileClick();
+}
