5a06e24957825bdd393f52646cb6b974ac20acd0
Bernat Gomez <bernat.gomez@mobetia.com>
2017-04-22 18:28:00 +0200

    Crear search presenter

---
+++ mobile/src/main/java/com/begoandapps/padelapp/presenter/SearchByNamePresenter.java
@@ -0,0 +1,68 @@
+package com.begoandapps.padelapp.presenter;
+
+import com.begoandapps.padel.usecases.search.SearchByNameUseCase;
+import com.begoandapps.padelapp.view.interfaces.ISearchByNameView;
+import com.myapps.data.PlaceModel;
+import com.myapps.forms.SearchForm;
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import apimodels.ApiErrorModel;
+
+/**
+ * Created by bernatgomez on 22/4/17.
+ */
+
+public class SearchByNamePresenter extends BasePresenter<ISearchByNameView> {
+
+    protected SearchByNameUseCase usecase;
+
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+    @Inject
+    public SearchByNamePresenter(Bus bus, SearchByNameUseCase usecase) {
+        super(bus);
+
+        this.usecase = usecase;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+    public void getPlaces(String name) {
+        SearchForm form = new SearchForm();
+
+        form.setName(name);
+
+        this.getView().showLoading();
+
+        this.usecase.getPlaces(form);
+    }
+
+////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+    @Subscribe
+    public void onSuccess(ArrayList<PlaceModel> places) {
+        this.getView().hideLoading();
+
+        this.getView().onListSuccess(places);
+    }
+
+    @Subscribe
+    public void onError(ApiErrorModel error) {
+        this.getView().hideLoading();
+
+        this.manageError(error);
+    }
+}
