8b67e542025907532963ac4e16178987853970f9
Bernat Gomez <bernat.gomez@mobetia.com>
2017-04-23 17:11:36 +0200

    Create search result form

---
+++ common/src/main/java/com/myapps/forms/SearchResultForm.java
@@ -0,0 +1,18 @@
+package com.myapps.forms;
+
+
+/**
+ * Created by bernatgomez on 23/4/17.
+ */
+
+public class SearchResultForm extends BaseForm {
+
+    public SearchResultForm() {
+
+    }
+
+    @Override
+    public boolean validate() {
+        return false;
+    }
+}
