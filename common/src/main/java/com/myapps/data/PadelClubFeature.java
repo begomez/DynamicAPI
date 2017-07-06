bd6b79b67ef80ca1c425704afb24158dfc10f2f8
Bernat Gomez <bernat.gomez@mobetia.com>
2017-07-06 20:11:19 +0200

    Config auto value in common module

--- common/src/main/java/com/myapps/data/PadelClubFeature.java
+++ common/src/main/java/com/myapps/data/PadelClubFeature.java
@@ -1,10 +1,16 @@
 package com.myapps.data;
 
 
+import com.google.auto.value.AutoValue;
+
 /**
  * Created by bernatgomez on 6/7/17.
  */
-
-public class PadelClubFeature extends BaseModel {
-
+@AutoValue
+public abstract class PadelClubFeature {
+    public abstract String title();
+    public abstract int icon();
+    public static PadelClubFeature create(String title, int icon) {
+        return new AutoValue_PadelClubFeature(title, icon);
+    }
 }
